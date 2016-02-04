dc4930908f66de51f5ed34f79529753e28970958
Caverntwo <caverntwo@users.noreply.github.com>
2016-02-04 20:52:32 +0100

    Initial Server Commit
    First Commit, created Eclipse-Project JServer, Added Base Structure of
    Java Server

---
+++ JServer/src/at/caverntwo/nimserver/JServerMain.java
@@ -0,0 +1,46 @@
+package at.caverntwo.nimserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class JServerMain {
+
+	public static void main(String[] args)
+	{
+		StartServerThread(10110);
+	}
+	
+	public static void StartServerThread(int port)
+	{
+		System.out.println("Starting Server with port " + port + "!");
+		ServerSocket socket = null;
+		try
+		{
+			socket = new ServerSocket(port);
+		}
+		catch (IOException ex)
+		{
+			ex.printStackTrace();
+		}
+		System.out.println("Server started up successfully!");
+		
+		while (true)
+		{
+			try
+			{
+				Socket connection = socket.accept();
+				if (connection.isConnected())
+				{
+					System.out.println("Client successfully connected, starting game...");
+					Thread gameInstance = new Thread(new JServerGameInstance(connection));
+					gameInstance.start();					
+				}
+			}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+	}
+}
