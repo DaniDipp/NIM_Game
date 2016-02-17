d5b546a40f7d229eca231c30cceb6d5e505ba224
Caverntwo <caverntwo@gmail.com>
2016-02-17 20:08:34 +0100

    Fixed Error in CommunicationHandler, internal (nested) class Encoder.Decoder done properly
    -Fixed ButtonStates decoded within the CommunicationHandler being wrong.
    -Encoder.Decoder interal (nested) class now done properly
    -Tester and CommunicationHandler still include testing code

---
+++ Communication/src/at/caverntwo/communication/CommunicationHandler.java
@@ -0,0 +1,50 @@
+package at.caverntwo.communication;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class CommunicationHandler 
+{	
+	private Socket socket;
+	private PrintWriter out;
+	
+	public CommunicationHandler(Socket socket_) throws Exception
+	{
+		socket = socket_;
+		//out = new PrintWriter(socket.getOutputStream());
+	}
+	
+	private void send(String s)
+	{
+//		out.write(s);
+		System.out.println(s);
+	}
+	
+	public void SendButtonStates(boolean[][] buttonStates)
+	{
+		StringBuffer sb = new StringBuffer();
+		for (byte i = 0; i < buttonStates.length; i++)
+		{
+			for (byte j = 0; j < buttonStates[i].length; j++)
+			{
+				sb.append(Encoder.EncodeState(i, j, buttonStates[i][j]));
+			}
+		}
+		send(sb.toString());
+	}
+	
+	public boolean[][] RecieveButtonStates(String decode)
+	{
+		String[] bs = decode.split(";");
+		boolean[][] buttonStates = {{false}, {false, false, false}, {false, false, false, false, false}, {false, false, false, false, false, false, false}}; //tree initialization
+		for (int i = 0; i < bs.length; i++)
+		{
+			System.out.println(bs[i]);
+			Encoder.DecodeState(bs[i]);
+			System.out.println("dec: row: " + Encoder.decoder.row + ", coloumn: " + Encoder.decoder.coloumn + ", enabled: " + Encoder.decoder.enabled);
+			buttonStates[Encoder.decoder.row][Encoder.decoder.coloumn] = Encoder.decoder.enabled;
+		}
+		return buttonStates;
+	}	
+}
--- Communication/src/at/caverntwo/communication/Tester.java
+++ Communication/src/at/caverntwo/communication/Tester.java
@@ -17,11 +17,30 @@ public class Tester {
 		System.out.println(encoder.EncodeState((byte) 1, (byte) 4, true));
 		
 		System.out.println(encoder.DecodeMessage("mHörmann;"));
-		encoder.DecodeState("s12:e;");
+		Encoder.DecodeState("s12:e;");
 		System.out.println("Row: " + Encoder.decoder.row + ", Coloumn: " + Encoder.decoder.coloumn + ", Enabled: " + Encoder.decoder.enabled);
-		encoder.DecodeState("s02:d;");
+		Encoder.DecodeState("s02:d;");
 		System.out.println("Row: " + Encoder.decoder.row + ", Coloumn: " + Encoder.decoder.coloumn + ", Enabled: " + Encoder.decoder.enabled);
-		encoder.DecodeState("s12:e;");
+		Encoder.DecodeState("s12:e;");
 		System.out.println("Row: " + Encoder.decoder.row + ", Coloumn: " + Encoder.decoder.coloumn + ", Enabled: " + Encoder.decoder.enabled);
+		
+		try {
+			CommunicationHandler c = new CommunicationHandler(null);
+			c.SendButtonStates(new boolean[][] {{false}, {false, true, false}, {false, false, false, true, false}, {false, true, false, false, false, false, true}});
+			boolean[][] sd = c.RecieveButtonStates("s00:d;s10:d;s11:e;s12:d;s20:d;s21:d;s22:d;s23:e;s24:d;s30:d;s31:e;s32:d;s33:d;s34:d;s35:d;s36:e;");
+//			boolean[][] sd = c.RecieveButtonStates("s00:d;");
+			for (int i = 0; i < sd.length; i++)
+			{
+				for (int j = 0; j < sd[i].length; j++)
+				{
+					System.out.print(sd[i][j] + " ");
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
