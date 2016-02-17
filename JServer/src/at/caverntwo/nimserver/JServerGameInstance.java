e0eb84c7a742b67c0dc005526acd7b9f3cd549d9
DaniDipp <dippigames@gmail.com>
2016-02-17 10:51:06 +0100

    Implemented the Method heads and added some documentation

--- JServer/src/at/caverntwo/nimserver/JServerGameInstance.java
+++ JServer/src/at/caverntwo/nimserver/JServerGameInstance.java
@@ -9,6 +9,7 @@ public class JServerGameInstance implements Runnable
 	
 	private boolean[][] buttonStates = {{true}, {true, true, true}, {true, true, true, true, true}, {true, true, true, true, true, true, true}};
 	private boolean[][] oldButtonStates =  {{true}, {true, true, true}, {true, true, true, true, true}, {true, true, true, true, true, true, true}};
+	private boolean gameInProgress = true;
 	
 	public JServerGameInstance(Socket connection, String name) {
 		this.socket = connection;
@@ -19,13 +20,39 @@ public class JServerGameInstance implements Runnable
 	public void run() {
 		//do the game stuff
 		
-//		preGameInit();
-//		while(gameInProgress){
-//			recieveLastRound();
-//			playRound();
-//			endRound();
-//		}
-//		closeConnection();
+		preGameInit();
+		while(gameInProgress ){
+			recieveLastRound();
+			playRound();
+			endRound();
+		}
+		closeConnection();
+	}
+
+	private void preGameInit() {
+		// TODO Sending Server information
+		// TODO Setting up game variables
+		
+	}
+
+	private void recieveLastRound() {
+		// TODO Recieve last rounds information from Client
+		
+	}
+
+	private void playRound() {
+		// TODO Calculate the best moves and perform them
+		
+	}
+
+	private void endRound() {
+		// TODO Send performed moves to Client
+		
+	}
+
+	private void closeConnection() {
+		// TODO End Game and close Connection to Client
+		
 	}
 
 	
